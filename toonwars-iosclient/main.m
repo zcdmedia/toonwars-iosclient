c53b017904acb9bb621c54588637c49a779fceda
Matthew Hinkle <matthewhinkle@me.com>
2012-05-06 16:23:33 -0700

    initial project

---
+++ toonwars-iosclient/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  toonwars-iosclient
+//
+//  Created by Matthew Hinkle on 5/6/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    return UIApplicationMain(argc, argv, nil, @"AppController");
+}
---
+++ toonwars-iosclient/toonwars-iosclient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  toonwars-iosclient
+//
+//  Created by Matthew Hinkle on 5/6/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
